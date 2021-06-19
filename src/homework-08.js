4529aa7b5f89439188c3fe498661fa7e5ac6d7b6
Vladimir-Taratushka <woltrr@gmail.com>
2021-06-19 15:34:13 +0300

    add event code esc

--- src/homework-08.js
+++ src/homework-08.js
@@ -3,7 +3,7 @@ import images from './gallery-items.js'
 
   const refs ={
       galleryList :document.querySelector(".js-gallery"),
-      lightBoxContainer : document.querySelector('.js-lightbox'),
+      lightBoxContainer : document.querySelector('.lightbox'),
       closeLightBox: document.querySelector('.lightbox__button'),
       galleryImage : document.querySelector('.gallery__image'),
       currentImage: document.querySelector('.lightbox__image')
@@ -31,23 +31,44 @@ function createListOfImagesMarkup(images) {
 };
 const imagesMarkup = createListOfImagesMarkup(images);
 refs.galleryList.insertAdjacentHTML('afterbegin',imagesMarkup);
-
 refs.galleryList.addEventListener('click',onGalleryListClick);
 refs.closeLightBox.addEventListener('click',onLightBoxClose);
 
+
+// refs.lightBoxContainer .addEventListener('click',onBackdropClick);
+
 function onGalleryListClick(event){
     event.preventDefault();
+   
     if(event.target.classList.contains('gallery__image')){
-      refs.lightBoxContainer.classList.add("is-open");  
+       window.addEventListener('keydown', onEscKeyPress);
+       refs.lightBoxContainer.classList.add("is-open");  
       return refs.currentImage.src = event.target.dataset.source;  
     
 }
 }
 
 function onLightBoxClose (event) {
+    window.removeEventListener('keydown', onEscKeyPress);
     refs.lightBoxContainer.classList.remove("is-open");
+    
 }
 
-function ononLightBoxClick (event){
-    console.log(event.target)
+
+function onEscKeyPress (event){
+    if(event.code === 'Escape'){
+    onLightBoxClose ();
+    console.log(event);
 }
+}
+// function onBackdropClick (event){
+
+    
+//     console.log(event.currentTarget);
+//     console.log(event.target);
+    
+//     if(event.currentTarget === event.target ){
+//     console.log('Клик именно  в backdrop');
+//     onLightBoxClose();
+// }
+// }
